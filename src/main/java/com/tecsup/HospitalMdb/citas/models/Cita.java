98a3b9beca44b008a9cb41221149b0b6c53463af
JuanAguirre10 <juancho71096@gmail.com>
2025-10-22 22:52:26 -0500

    correcion

--- src/main/java/com/tecsup/HospitalMdb/citas/models/Cita.java
+++ src/main/java/com/tecsup/HospitalMdb/citas/models/Cita.java
@@ -1,26 +1,22 @@
 package com.tecsup.HospitalMdb.citas.models;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
-@Document(collection = "citas")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Document(collection = "citas")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Cita {
-
     @Id
     private String id;
     private String idPaciente;
     private String idMedico;
     private LocalDate fecha;
-    private LocalTime hora;
+    private String hora;
     private String motivo;
     private String estado;
 }
